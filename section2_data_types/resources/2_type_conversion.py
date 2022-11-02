9eede6f0ec7afee2879cc5f09add43a69d73227c
chaiyatorn <chaiyatorn.nr@gmail.com>
2022-11-02 22:10:51 +0700

    add section 2

---
+++ section2_data_types/resources/2_type_conversion.py
@@ -0,0 +1,19 @@
+""" Type Conversion """
+
+# Type Error
+# len(100)
+# "Hello" + 10
+
+# Type Check
+print(type(100))
+print(type(15.123))
+print(type("Hello"))
+print(type(True))
+
+# Type Conversion
+print(type(str(100)))
+print(type(str("1.2345")))
+print(type(int("1")))
+print(type(float("1.123")))
+# print(type(int("1.123")))
+print(type(int("asdqw")))
---
+++ section2_data_types/resources/4_number_manipulation_and_fstring.py
@@ -0,0 +1,13 @@
+""" Number Manipulation """
+
+print(8 / 3)
+print(type(8 / 3))
+print(round(8 / 3))
+print(type(round(8 / 3)))
+print(8 // 3)
+print(type(8 // 3))
+
+
+score = 3
+score += 5
+print(f"new score is {score}")
