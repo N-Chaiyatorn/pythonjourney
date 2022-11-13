0be92f18b9c36c5cdc40f3dce76c03002e60729b
N-Chaiyatorn <chaiyatorn.nr@gmail.com>
2022-11-13 15:12:19 +0700

    add section 5

---
+++ section5_loops/resources/1_loop_on_lists.py
@@ -0,0 +1,6 @@
+""" Loop on List """
+
+car_brands = ["ford", "toyota", "honda"]
+
+for car_brand in car_brands:
+    print(car_brand)
---
+++ section5_loops/resources/2_for_loop_with_range.py
@@ -0,0 +1,6 @@
+""" For Loop with List """
+
+# range(start, step, stop)
+# start from start, doesn't include stop
+for number in range(0, 10):
+    print(number)
