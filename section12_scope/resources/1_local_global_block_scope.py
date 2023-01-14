d138d59b36c3d71b657083522205bca180c5f020
chaiyatorn <chaiyatorn.nr@gmail.com>
2023-01-14 15:33:48 +0700

    add section12

---
+++ section12_scope/resources/1_local_global_block_scope.py
@@ -0,0 +1,40 @@
+# Local Scope
+
+# def local_scope():
+#     y = 2
+#     print(y)
+# local_scope()
+# print(y)
+
+
+
+# Global Scope
+
+# a = 1
+# def global_scope():
+#     b = 2
+#     print(a)
+# global_scope()
+
+
+
+# Block scope
+
+# def block_scope():
+#     a = 1
+#     if True:
+#         b = a
+#     print(b)
+# block_scope()
+
+
+
+# Extra: modify global scope
+
+# a = 1
+# def modify_global_scope():
+#     global a
+#     a += 2
+#     print(a)
+# modify_global_scope()
+# print(a)
