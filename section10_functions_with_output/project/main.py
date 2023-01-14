b3521e7d76eb550a5cac60002017a1ae65029704
chaiyatorn <chaiyatorn.nr@gmail.com>
2023-01-14 15:17:20 +0700

    add section10

---
+++ section10_functions_with_output/project/main.py
@@ -0,0 +1,21 @@
+#Calculator
+def add(n1, n2):
+  #TODO
+
+def subtract(n1, n2):
+  #TODO
+
+def multiply(n1, n2):
+  #TODO
+
+def divide(n1, n2):
+  #TODO
+
+#TODO map the user operand input to the functions above
+operations = {
+  "+": "todo",
+  "-": "todo",
+  "*": "todo",
+  "/": "todo",
+}
+
