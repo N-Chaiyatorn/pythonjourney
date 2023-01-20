814dc6104d402afeb87a981fe06a7e514fc0767d
Pasit065 <pan289277@gmail.com>
2023-01-20 13:40:52 +0700

    leap years days

--- section10_functions_with_output/assignments/1_days_in_month.py
+++ section10_functions_with_output/assignments/1_days_in_month.py
@@ -16,16 +16,21 @@ def is_leap(year):
   if year % 4 == 0:
     if year % 100 == 0:
       if year % 400 == 0:
-        print("Leap year.")
+        return True
       else:
-        print("Not leap year.")
+        return False
     else:
-      print("Leap year.")
+      return True
   else:
-    print("Not leap year.")
+    return False
 
-def days_in_month():
-  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
+def days_in_month(each_year, month):
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  isthisaleap = is_leap(year = each_year) 
+  if isthisaleap:
+    month_days[1] = 29
+
+  return month_days[month-1]
   
   
 #🚨 Do NOT change any of the code below 
