f745df19f093af4da028e200d22fe57fe25c176d
Pasit065 <pan289277@gmail.com>
2023-01-22 16:44:05 +0700

    section12 commit

--- section12_scope/resources/1_local_global_block_scope.py
+++ section12_scope/resources/1_local_global_block_scope.py
@@ -1,20 +1,20 @@
 # Local Scope
 # def local_scope():
 #     y = 2
-#     print(y)
+#     print(y)                  ===> result is 2
 # local_scope()
-# print(y)
+# print(y)                       ===> result is error
 
 
 # Global Scope
-# a = 1
+# a = 1 (global scope)
 # def global_scope():
 #     b = 2
-#     print(a)
+#     print(a)   (local scope)                 ===> result is 2
 # global_scope()
 
 
-# Block scope
+# Block scope   ====>ในภาษาอื่น คือ {}    เเต่ python ไม่มี block scope
 # def block_scope():
 #     a = 1
 #     if True:
@@ -26,12 +26,12 @@
 # Extra: modify global scope
 # a = 1
 # def modify_global_scope():
-#     global a
+#     global a                      
 #     a += 2
 #     print(a)
 # modify_global_scope()
-# print(a)
+# print(a)              ====> result is 3
 
-# Global constant
+# Global constant    ====> ค่าคงที่ที่ไม่ควรจะเปลี่ยนเเปลงเลย เราควรจะประกาศชื่อตัวเเปรให้เป็นตัวพิมพ์ใหญ่ทั้งหมด
 # PI = 3.14159
 # GOOGLE_URL = "www.google.com"
