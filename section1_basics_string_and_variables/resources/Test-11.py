7605effff2ca73af8ad069cd6889bea2f6cbdf75
Pasit065 <pan289277@gmail.com>
2023-01-25 23:01:47 +0700

    commit

---
+++ section1_basics_string_and_variables/resources/Test-11.py
@@ -0,0 +1,13 @@
+
+
+# def aiming(test):
+#     a = test + 4
+#     return a
+
+# a = int(input("Type your number: "))
+# aiming(test = a)
+# print(a)
+
+what = int(input("Type: "))
+for letter in range(what):
+    print(letter)
