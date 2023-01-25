35e799cafb127222a28daf0ebee4e4b8c23a9b15
Pasit065 <pan289277@gmail.com>
2023-01-25 21:39:29 +0700

    sent assignment13

--- section13_debugging/assignments/1_debugging.py
+++ section13_debugging/assignments/1_debugging.py
@@ -1,14 +1,14 @@
 # 1st question
 # number = int(input("Which number do you want to check?"))
 
-# if number % 2 = 0:
+# if number % 2 == 0:
 #   print("This is an even number.")
 # else:
 #   print("This is an odd number.")
   
 
 # 2nd question
-# year = input("Which year do you want to check?")
+# year = int(input("Which year do you want to check?"))
 # if year % 4 == 0:
 #   if year % 100 == 0:
 #     if year % 400 == 0:
@@ -22,12 +22,12 @@
   
 
 # 3rd question
-# for number in range(1, 101):
-#   if number % 3 == 0 or number % 5 == 0:
-#     print("FizzBuzz")
-#   if number % 3 == 0:
-#     print("Fizz")
-#   if number % 5 == 0:
-#     print("Buzz")
-#   else:
-#     print([number])
+for number in range(1, 101):
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+  elif number % 3 == 0:
+    print("Fizz")
+  elif number % 5 == 0:
+    print("Buzz")
+  else:
+    print(number)
