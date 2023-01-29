393366d6435991232198ae843292d4ca0d71c9da
Pasit065 <pan289277@gmail.com>
2023-01-29 23:36:49 +0700

    Resend Project section10

--- section10_functions_with_output/project/main.py
+++ section10_functions_with_output/project/main.py
@@ -34,11 +34,20 @@ def operating_print(n1, operator, n2, n3):
   '''
   print(f"{n1} {operator} {n2} = {n3}")
 
-is_continous = True
+def is_user_finished(user_ans):
+  while True:
+    if user_ans  == 'y':
+      return True
+    elif user_ans  == 'n':
+      return False
+    else:
+      user_ans = input(f"Error!! you type incorrect way , please type 'y' to continous or type 'n' to exit.: ")
+
+is_not_finished = True
 number1 = int(input("What's the first number?: "))      # Ask user to type first number 
 for symbol in operations:
   print(symbol)
-while is_continous:
+while is_not_finished:
   operator = input("Pick an operation: ")               
   number2 = int(input("What's the next number?: "))
   while True:
@@ -56,16 +65,12 @@ while is_continous:
       break
     else:
       operator = input("Error!! Its seems you type incorrect operator so please try again: ")
-      
+
   operating_print(number1, operator, number2, number3)
-  ask_user = input(f"Type 'y' to continous calculating with {number3}, or type 'n' to exit.: ")
+  user_ans_input = input(f"Type 'y' to continous calculating with {number3}, or type 'n' to exit.: ")
+  number1 = number3
+  is_not_finished = is_user_finished(user_ans = user_ans_input)
 
-  if ask_user == 'y':
-    number1 = number3
-    is_continous = True
-
-  elif ask_user == 'n':
-    is_continous = False
   
 
 
