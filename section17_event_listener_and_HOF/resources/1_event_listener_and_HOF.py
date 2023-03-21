39336b3544ab832b4972cbe3d5a7524706946a70
Pasit065 <pan289277@gmail.com>
2023-03-21 21:07:09 +0700

    commit for sure

--- section17_event_listener_and_HOF/resources/1_event_listener_and_HOF.py
+++ section17_event_listener_and_HOF/resources/1_event_listener_and_HOF.py
@@ -3,40 +3,41 @@
 from turtle import Turtle, Screen
 
 tim = Turtle()
-screen = Screen()
+screen = Screen()       # screen
 
-def move_forward():
-    tim.forward(10)
-
-screen.listen()
-
-# Higher Order Function (HOF)
-screen.onkey(key="space", fun=move_forward)
-screen.exitonclick()
-
-
-# Challenge
 # def move_forward():
 #     tim.forward(10)
 
-# def move_backward():
-#     tim.backward(10)
+screen.listen()         # ตั้ง screen ไว้คอย listen 
 
-# def turn_left():
-#     tim.left(10)
-
-# def turn_right():
-#     tim.right(10)
-
-# def clear():
-#     tim.clear()
-#     tim.penup()
-#     tim.home()
-#     tim.pendown()
-
-# screen.onkey(key="w", fun=move_forward)
-# screen.onkey(key="s", fun=move_backward)
-# screen.onkey(key="a", fun=turn_left)
-# screen.onkey(key="d", fun=turn_right)
-# screen.onkey(key="c", fun=clear)
+# Higher Order Function (HOF)
+# คือ function ที่มี อีก function ในค่า input ของ function
+# screen.onkey(key="space", fun=move_forward)        # ข้อควรระวังไม่ควรใส่วงเล็บเข้าไปใน function ที่เรา nput เข้าไปเด็ดขาด     
 # screen.exitonclick()
+
+
+# Challenge
+def move_forward():
+    tim.forward(10)
+
+def move_backward():
+    tim.backward(10)
+
+def turn_left():
+    tim.left(10)
+
+def turn_right():
+    tim.right(10)
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+screen.onkey(key="w", fun=move_forward)
+screen.onkey(key="s", fun=move_backward)
+screen.onkey(key="a", fun=turn_left)
+screen.onkey(key="d", fun=turn_right)
+screen.onkey(key="c", fun=clear)
+screen.exitonclick()
