cf591d46bbe81e8f624749f0cdfecbf8f238400d
Pasit065 <pan289277@gmail.com>
2023-04-04 01:24:10 +0700

    Commit for sure

--- section17_event_listener_and_HOF/resources/1_event_listener_and_HOF.py
+++ section17_event_listener_and_HOF/resources/1_event_listener_and_HOF.py
@@ -12,7 +12,7 @@ screen.listen()         # ตั้ง screen ไว้คอย listen
 
 # Higher Order Function (HOF)
 # คือ function ที่มี อีก function ในค่า input ของ function
-# screen.onkey(key="space", fun=move_forward)        # ข้อควรระวังไม่ควรใส่วงเล็บเข้าไปใน function ที่เรา nput เข้าไปเด็ดขาด     
+# screen.onkey(key="space", fun=move_forward)        # ข้อควรระวังไม่ควรใส่วงเล็บเข้าไปใน function ที่เรา input เข้าไปเด็ดขาด     
 # screen.exitonclick()
 
 
@@ -34,10 +34,74 @@ def clear():
     tim.penup()
     tim.home()
     tim.pendown()
+    
 
+# screen.onkey(key="w", fun=move_forward)
+# screen.onkey(key="s", fun=move_backward)
+# screen.onkey(key="a", fun=turn_left)
+# screen.onkey(key="d", fun=turn_right)
+# screen.onkey(key="c", fun=clear)
+# screen.exitonclick()
+
+
+def typing_k():
+    tim.pendown()
+    tim.pencolor("red")
+    tim.right(45)
+    tim.forward(50)
+    tim.backward(50)
+    tim.left(90)
+    tim.forward(50)
+    tim.backward(50)
+    tim.left(135)
+    tim.forward(20)
+    tim.right(90)
+    tim.forward(50)
+    tim.backward(100)
+    tim.penup()
+    tim.home()
+    
+
+def typing_e():
+    tim.pendown()
+    tim.pencolor("red")
+    tim.forward(50)
+    tim.backward(50)
+    tim.left(90)
+    tim.forward(50)
+    tim.right(90)
+    tim.forward(50)
+    tim.backward(50)
+    tim.right(90)
+    tim.forward(100)
+    tim.left(90)
+    tim.forward(50)
+    tim.penup()
+    tim.home()
+
+def typing_i():
+    tim.pendown()
+    tim.pencolor("red")
+    tim.left(90)
+    tim.forward(50)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
+    tim.dot(5)
+    tim.penup()
+    tim.backward(10)
+    tim.pendown()
+    tim.backward(100)
+    tim.penup()
+    tim.home()
+
+tim.pendown()
 screen.onkey(key="w", fun=move_forward)
 screen.onkey(key="s", fun=move_backward)
 screen.onkey(key="a", fun=turn_left)
 screen.onkey(key="d", fun=turn_right)
 screen.onkey(key="c", fun=clear)
+screen.onkey(key="k", fun=typing_k)
+screen.onkey(key="e", fun=typing_e)
+screen.onkey(key="i", fun=typing_i)
 screen.exitonclick()
