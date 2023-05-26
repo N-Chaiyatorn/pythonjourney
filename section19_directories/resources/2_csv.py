c0167a431beda724a365a9122e213d5da531f46f
Pasit065 <pan289277@gmail.com>
2023-05-26 23:53:52 +0700

    Section19

--- section19_directories/resources/2_csv.py
+++ section19_directories/resources/2_csv.py
@@ -1,4 +1,6 @@
-# with open("transaction.csv") as data_file:
+# ไฟล์ csv (comma separated value) คือ ไฟล์ที่มีลักษณะเป็นตารางที่เเยกเป็นเเถวกับหลักโดยเเต่ละหลักในตารางจะถูกเเยกกันด้วย comma
+
+# with open("/Gittest/Python study/pythonjourney/section19_directories/resources/transaction.csv") as data_file:
 #     data = data_file.read()
 #     print(data)
 
@@ -6,14 +8,14 @@
 
 # import csv
 
-# with open("transaction.csv") as data_file:
-#     data = csv.reader(data_file)
+# with open("/Gittest/Python study/pythonjourney/section19_directories/resources/transaction.csv") as data_file:
+#     data = csv.reader(data_file)       # csv.reader จะทำหน้าที่สร้าง list เเต่ละเเถว  (list ซ้อน list)        
 #     for row in data:
 #         print(row)
 
 
 
-# import pandas
+import pandas #ค่อนข้างสำคัญ 
 
-# data = pandas.read_csv("transaction.csv")
-# print(data["unitsSold"])
+data = pandas.read_csv("/Gittest/Python study/pythonjourney/section19_directories/resources/transaction.csv")
+print(data["unitsSold"][4])
