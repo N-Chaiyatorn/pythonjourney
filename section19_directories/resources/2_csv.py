2498c16d6ba1394c8ddfd757717c683170f6d5c5
N-Chaiyatorn <77059917+N-Chaiyatorn@users.noreply.github.com>
2023-06-10 12:44:53 +0700

    Section19 (#12)
    * add section19
    
    * add projects
    
    * Section19
    
    * squirrel data
    
    * Delete 2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv
    
    * Sending project section19
    
    * 2018_squirrels_data
    
    * Sending U.S states game project
    
    ---------
    
    Co-authored-by: Pasit065 <pan289277@gmail.com>
    Co-authored-by: Pasit065 <116727423+Pasit065@users.noreply.github.com>

---
+++ section19_directories/resources/2_csv.py
@@ -0,0 +1,21 @@
+# ไฟล์ csv (comma separated value) คือ ไฟล์ที่มีลักษณะเป็นตารางที่เเยกเป็นเเถวกับหลักโดยเเต่ละหลักในตารางจะถูกเเยกกันด้วย comma
+
+# with open("/Gittest/Python study/pythonjourney/section19_directories/resources/transaction.csv") as data_file:
+#     data = data_file.read()
+#     print(data)
+
+
+
+# import csv
+
+# with open("/Gittest/Python study/pythonjourney/section19_directories/resources/transaction.csv") as data_file:
+#     data = csv.reader(data_file)       # csv.reader จะทำหน้าที่สร้าง list เเต่ละเเถว  (list ซ้อน list)        
+#     for row in data:
+#         print(row)
+
+
+
+import pandas #ค่อนข้างสำคัญ 
+
+data = pandas.read_csv("/Gittest/Python study/pythonjourney/section19_directories/resources/transaction.csv")
+print(data["unitsSold"][4])
---
+++ section19_directories/resources/3_pandas.py
@@ -0,0 +1,68 @@
+# https://pandas.pydata.org/docs/
+import pandas
+import csv
+
+# Dataframes and Series
+# data = pandas.read_csv("transaction.csv")
+# print(type(data))                       # The output is <class 'pandas.core.frame.DataFrame'> หมายถึงว่าตารางทั้งตารางในไฟล์ csv จะเป็น data type ที่มีชื่อว่า DataFrame
+# print(type(data["unitsSold"]))          # The output is <class 'pandas.core.series.series'>           หมายถึงว่า column ที่ชื่อว่า unitsSold ในไฟล์ csv จะเป็น data type ที่มีชื่อว่า series(column เเต่ละ column ในไฟล์ จะมี data type ชื่อว่า series)
+
+
+# average units sold and max units sold, using both non-pandas and pandas
+# 1.ใช้ pandas หา max
+# data = pandas.read_csv("/Gittest/Python study/pythonjourney/section19_directories/resources/transaction.csv")
+# print(data["unitsSold"].max())
+
+# 2.ใช้ pandas หา average            !!!!!!!
+# data = pandas.read_csv("/Gittest/Python study/pythonjourney/section19_directories/resources/transaction.csv")
+# units_num = data["unitsSold"].values
+# additional_num = 0
+# for each_num in units_num:
+#     additional_num += each_num
+# print(additional_num/len(units_num))
+
+# 3.ไม่ใช้ pandas หา max
+# with open("/Gittest/Python study/pythonjourney/section19_directories/resources/transaction.csv") as data_file:
+#     data = csv.reader(data_file)
+#     maximum_value = 0
+#     for each_row in data:
+#         if each_row[4] != "unitsSold" and int(each_row[4]) > maximum_value:
+#             maximum_value = int(each_row[4])
+#     print(maximum_value)
+
+          
+
+# 4.ไม่ใช้ pandas หา average
+# with open("/Gittest/Python study/pythonjourney/section19_directories/resources/transaction.csv") as data_file:
+#     data = csv.reader(data_file)
+#     additional_num = 0
+#     row = 0
+#     for each_row in data:
+#         if each_row[4] != "unitsSold":
+#             additional_num += int(each_row[4])
+#             row +=1
+# print(additional_num/row)
+
+
+
+
+
+
+# filter product sold = SUPA101 rows
+# ถ้าจะเลือกเเค่เเถวหนึ data.productSold 
+# print(data[data.productSold == "SUPA101"])
+
+
+# get row that contains highest units sold
+# data = pandas.read_csv("/Gittest/Python study/pythonjourney/section19_directories/resources/transaction.csv")
+# print(data[data["unitsSold"] == data["unitsSold"].max()])
+
+# create dataframe
+data_dict = {
+    "students": ["pan", "jean"],    #key เปรียบเสมือนชื่อของหลัก(ชื่อของหัวตาราง) เเละ  
+    "scores": [90, 90]
+}
+data = pandas.DataFrame(data_dict)
+print(data)
+
+data.to_csv("students.csv")
