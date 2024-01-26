a8be6f7f184da6c3eea2d1a33f3230f71c9395d6
Pasit065 <pan289277@gmail.com>
2024-01-26 19:47:43 +0700

    Sending flask challenge 2

--- section28_create_api_with_flask/main.py
+++ section28_create_api_with_flask/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, request, session, redirect, url_for
+from flask import Flask, jsonify, render_template, make_response, request, session, redirect, url_for, abort
 from flask_sqlalchemy import SQLAlchemy
 import os
 import random
@@ -32,6 +32,14 @@ This will install the packages from requirements.txt for this project.
 # it will connect to cafes.db (2) in C:\instance instead.
 
 app = Flask(__name__)
+
+api_secret_key = {
+    "topSecretKey":"Gojovssukuna23"
+}
+
+
+
+
 os.chdir("C:\Gittest\python_learning_after_sec_21\pythonjourney\section28_create_api_with_flask")
 app.secret_key = secrets.token_hex()
 
@@ -117,11 +125,84 @@ def get_particular_loc_cafes():
 ## HTTP GET - Read Record
 
 ## HTTP POST - Create Record
+@app.route("/add-cafe/<int:cafe_id>", methods = ["POST"])
+def add_new_cafe(cafe_id):
+    # Get user's json parameters.
+    json_params = request.get_json()
+    json_params["id"] = cafe_id
+
+    # Create new row that consist our json parameters with specific cafe id.
+    new_cafe = Cafe(**json_params)
+
+    # Add new row to database.
+    db.session.add(new_cafe)
+    db.session.commit()
+
+    returned_text = """Successful add for\n\n"""
+
+    for col_name in json_params:
+        returned_text += f"{col_name}:{json_params[col_name]}\n"
+
+    return returned_text
 
 ## HTTP PUT/PATCH - Update Record
+@app.route("/update-price/<int:cafe_id>", methods = ["PATCH"])
+def update_new_price(cafe_id):
+    # Get specific row from cafe_id input value.
+    selected_cafe = db.get_or_404(Cafe, cafe_id)
+    old_coffee_price = selected_cafe.coffee_price
 
-## HTTP DELETE - Delete Record
+    # Get json parameters.
+    json_params = request.get_json()
 
+    # Check if 'coffee_price' didn't exist in parameters, it will return error json message.
+    if "coffee_price" not in json_params:
+        return jsonify(
+            {
+                "error":"new 'coffee_price' are required."
+            }
+        )
+    
+    # Assign new price to determined row.
+    selected_cafe.coffee_price = f"£{str(json_params['coffee_price'])}"
+    db.session.commit()
+
+    return f"Successful edit for {selected_cafe.id} {selected_cafe.name}.\nbefore:{old_coffee_price}$.\nAfter editing:{selected_cafe.coffee_price}."
+
+@app.route("/update/<int:cafe_id>", methods = ["PATCH"])
+def update_specific_column(cafe_id):
+    '''In this case we will editing multiple column in each row.'''
+    # Get parameters from users and selected row from cafe table.
+    json_params = request.get_json()
+    selected_cafe = db.get_or_404(Cafe, cafe_id)
+
+    # Set new value that have been determined by json_params into selected row. 
+    for param_name in json_params:
+        selected_cafe.__setattr__(param_name, json_params[param_name])
+
+    db.session.commit()
+
+    return f"Edit data for {cafe_id} has been done."
+
+# HTTP DELETE - Delete Record
+@app.route("/delete-cafe/<int:cafe_id>", methods = ["DELETE"])
+def delete_specific_cafe(cafe_id):
+    # Check header's parameters if users type incorrect api key we will return error text with 403 status code.
+    if request.headers.get("x-api-key") != api_secret_key["topSecretKey"]:
+        return jsonify(
+            {
+                "status_code":403,
+                "error":"Invalid token attemp."
+            }
+        ), 403
+
+    # Get specific cafe from cafe_id.
+    selected_cafe = db.get_or_404(Cafe, cafe_id) 
+    # Delete determined row.
+    db.session.delete(selected_cafe)
+    db.session.commit()
+
+    return f"Deleting id:{cafe_id} process has already successful"
 
 if __name__ == '__main__':
     app.run(debug=True)
